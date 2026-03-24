1fd01e7887f719084426f7d7116b5ea89b5f434b
avila199817 <cristian@onionsupport.com>
2026-03-24 22:11:06 +0100

    Create en.js

---
+++ js/i18n/en.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function(){
+
+  window.Onion = window.Onion || {};
+  Onion.i18n = Onion.i18n || {};
+  Onion.i18n.en = {
+
+    panel: "Dashboard",
+    incidencias: "Incidents",
+    clientes: "Clients",
+    cuenta: "Account",
+    ajustes: "Settings",
+    buscar: "Search...",
+    cerrarSesion: "Log out"
+
+  };
+
+})();

51c4d6d3172330d695a766a20fe8fe71d7f1f9e7
avila199817 <cristian@onionsupport.com>
2026-04-06 13:45:19 +0200

    Update index.js

--- js/features/facturas/index.js
+++ js/features/facturas/index.js
@@ -410,7 +410,17 @@ function render(items){
   <td class="col-actions">
     <div class="actions">
       <button class="btn-action view" data-id="${d.id}">Ver</button>
-      <button class="btn-action download" data-id="${d.id}">PDF</button>
+      <button class="btn-action download" data-id="${d.id}" aria-label="Descargar PDF">
+        <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd"
+            d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z"
+            clip-rule="evenodd"/>
+          <path fill-rule="evenodd"
+            d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.
+            685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z"
+            clip-rule="evenodd"/>
+        </svg>
+      </button>
       ${
         d.estadoPago.raw === "pendiente"
           ? `<button class="btn-action pay" data-id="${d.id}">Pagar</button>`

693ae29a2cf446382d5774a8f10e0eec48b03f74
avila199817 <cristian@onionsupport.com>
2026-04-06 13:48:58 +0200

    Update index.js

--- js/features/facturas/index.js
+++ js/features/facturas/index.js
@@ -409,7 +409,13 @@ function render(items){
 
   <td class="col-actions">
     <div class="actions">
-      <button class="btn-action view" data-id="${d.id}">Ver</button>
+      <button class="btn-action send" data-id="${d.id}" aria-label="Enviar factura">
+        <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+          <path d="m3.62 6.389 8.396 6.724 8.638-6.572-7.69-4.29a1.975 1.975 0 0 0-1.928 0L3.62 6.39Z"/>
+          <path d="m22 8.053-8.784 6.683a1.978 1.978 0 0 1-2.44-.031L2.02 7.693a1.091 1.091 0 0 0-.019.
+          199v11.065C2 20.637 3.343 22 5 22h14c1.657 0 3-1.362 3-3.043V8.053Z"/>
+        </svg>
+      </button>
       <button class="btn-action download" data-id="${d.id}" aria-label="Descargar PDF">
         <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
           <path fill-rule="evenodd"

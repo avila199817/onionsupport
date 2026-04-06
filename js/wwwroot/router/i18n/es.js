3d4ef3c0ac7805f16d5275289bd13cc52dab425b
avila199817 <cristian@onionsupport.com>
2026-04-06 17:07:45 +0200

    Create es.js

---
+++ js/wwwroot/router/i18n/es.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function(){
+
+  window.Onion = window.Onion || {};
+  Onion.i18n = Onion.i18n || {};
+  Onion.i18n.es = {
+
+    Panel: "Panel",
+    Incidencias: "Incidencias",
+    Clientes: "Clientes",
+    Cuenta: "Cuenta",
+    Ajustes: "Ajustes",
+    Buscar: "Buscar...",
+    CerrarSesion: "Cerrar sesión"
+
+  };
+
+})();

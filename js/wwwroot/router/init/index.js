7c9c7acf8562b72044f5b0b2f478fb059d3ed1ca
avila199817 <cristian@onionsupport.com>
2026-04-06 17:38:14 +0200

    Update index.js

--- js/wwwroot/router/init/index.js
+++ js/wwwroot/router/init/index.js
@@ -1,1 +1,34 @@
+"use strict";
 
+(function(){
+
+  if(!window.Onion){
+    console.error("Onion no existe (init/index)");
+    return;
+  }
+
+  const Onion = window.Onion;
+
+  // evitar doble ejecución
+  if(Onion.__initModuleLoaded__) return;
+  Onion.__initModuleLoaded__ = true;
+
+  /* =========================================================
+     VALIDAR MÓDULOS
+  ========================================================= */
+
+  if(!Onion.init){
+    console.warn("⚠️ init.js no cargado");
+  }
+
+  if(!Onion.boot){
+    console.warn("⚠️ boot.js no cargado");
+  }
+
+  /* =========================================================
+     READY
+  ========================================================= */
+
+  console.log("🚀 Init módulo listo");
+
+})();

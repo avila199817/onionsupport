2a332c88fabf499350948272c7dbfe4ae2bd3b3f
avila199817 <cristian@onionsupport.com>
2026-04-11 23:25:53 +0200

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -319,6 +319,7 @@ export const App = (() => {
         syncUserUI,
         warmup,
         navigateAfterSessionRestore,
+
         applyPostRenderLoaderPolicy: () =>
           applyPostRenderLoaderPolicy({
             AppCore,

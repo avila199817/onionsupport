e03d73db0c8cf53746db10dcaa0c6ff444c3ec82
avila199817 <cristian@onionsupport.com>
2026-04-11 20:52:57 +0200

    Create constants.js

---
+++ src/app/constants.js
@@ -0,0 +1,13 @@
+/* =========================================================
+   Onion SPA - App Constants
+   Archivo: src/app/constants.js
+
+   Responsabilidades:
+   - centralizar constantes del bootstrap de la app
+   - definir scope global de cleanup
+   - definir timeouts de failsafe del boot
+========================================================= */
+
+export const APP_SCOPE = "app:global";
+
+export const BOOT_FAILSAFE_LOADER_MS = 2500;

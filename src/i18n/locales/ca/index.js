af62d82afeb774878ff288dad305372720c37974
avila199817 <cristian@onionsupport.com>
2026-04-12 17:05:37 +0200

    Update index.js

--- src/i18n/locales/ca/index.js
+++ src/i18n/locales/ca/index.js
@@ -1,1 +1,10 @@
+/* =========================================================
+   Onion SPA - I18n Català Index
+   Archivo: src/i18n/locales/ca/index.js
 
+   Responsabilidades:
+   - reexportar el locale catalán
+   - mantener un punto de entrada limpio para ca
+========================================================= */
+
+export { ca as default, ca } from "./ca.js";

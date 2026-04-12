3be74dffa276303a5d2e95c5ea15fc3ee29bbeab
avila199817 <cristian@onionsupport.com>
2026-04-12 04:47:52 +0200

    Update index.js

--- src/views/facturas/index.js
+++ src/views/facturas/index.js
@@ -1,10 +1,53 @@
 /* =========================================================
-   Onion SPA - Facturas Module Index
+   Onion SPA - Facturas Module Index (FULL PRO SAAS PANEL)
    Archivo: src/views/facturas/index.js
 
    Responsabilidades:
    - punto de entrada del módulo de facturas
-   - reexportar la vista principal
+   - reexportar vista principal
+   - reexportar helpers de modelo
+   - dejar API pública del módulo preparada para crecer
 ========================================================= */
 
 export { FacturasView } from "./facturasView.js";
+
+export {
+  truncate,
+  formatMoney,
+  formatDate,
+  formatDateTime,
+  formatRelativeDate,
+  getInitials,
+  normalizeEstadoPago,
+  normalizeEstado,
+  getEstadoPagoLabel,
+  getEstadoLabel,
+  getEstadoPagoChipStyle,
+  getEstadoChipStyle,
+  getFacturaNumero,
+  getFacturaFecha,
+  getFacturaUpdatedAt,
+  getFacturaClienteNombre,
+  getFacturaClienteEmpresa,
+  getFacturaClienteEmail,
+  getFacturaPreview,
+  getFacturaCurrency,
+  getFacturaTotal,
+  getFacturaBaseImponible,
+  getFacturaImpuestosTotal,
+  getFacturaDescuentoTotal,
+  isFacturaPaid,
+  isFacturaPending,
+  isFacturaOverdue,
+  normalizeFactura,
+  extractFacturas,
+  extractNormalizedFacturas,
+  getRemoteCount,
+  extractStats,
+  sumFacturasTotal,
+  sumFacturasBase,
+  countFacturasByEstadoPago,
+  countFacturasByEstado,
+  sortFacturas,
+  filterFacturas,
+} from "./facturas.model.js";

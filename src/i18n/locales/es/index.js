d29e01d0e69cb2b8719bc250e745e37feb014cd3
avila199817 <cristian@onionsupport.com>
2026-04-12 17:10:06 +0200

    Update index.js

--- src/i18n/locales/es/index.js
+++ src/i18n/locales/es/index.js
@@ -1,1 +1,11 @@
+/* =========================================================
+   Onion SPA - I18n Español Index
+   Archivo: src/i18n/locales/es/index.js
 
+   Responsabilidades:
+   - reexportar locale español
+   - punto de entrada limpio para es
+   - compatibilidad con import default y named
+========================================================= */
+
+export { es as default, es } from "./sidebar.js";

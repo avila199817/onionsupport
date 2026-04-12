d96cf4802fbb8a29420246a6a73c03c34f33466b
avila199817 <cristian@onionsupport.com>
2026-04-12 02:53:12 +0200

    Update index.js

--- src/views/facturas/index.js
+++ src/views/facturas/index.js
@@ -1,1 +1,11 @@
+/* =========================================================
+   Onion SPA - Facturas View Index
+   Archivo: src/views/facturas/index.js
 
+   Responsabilidades:
+   - punto de entrada del módulo FacturasView
+   - reexportar la vista de facturas
+   - permitir imports limpios desde la carpeta
+========================================================= */
+
+export { FacturasView } from "./facturasView.js";

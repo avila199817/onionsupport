cca1d3f987837f0482f846a6bd86d195d0be5e24
avila199817 <cristian@onionsupport.com>
2026-04-15 14:26:31 +0200

    Update index.js

--- src/views/password-reset/confirm/index.js
+++ src/views/password-reset/confirm/index.js
@@ -1,1 +1,18 @@
+/* =========================================================
+   Onion SPA - Reset Password Confirm Entry
+   Archivo: src/views/password-reset/confirm/index.js
 
+   Responsabilidades:
+   - exponer la vista confirm del reset password
+   - delegar la orquestación en confirmView.js
+   - mantener compatibilidad default + named export
+   - entrypoint limpio del submódulo confirm
+========================================================= */
+
+import ConfirmResetPasswordView from "./confirmView.js";
+
+export {
+  ConfirmResetPasswordView,
+};
+
+export default ConfirmResetPasswordView;

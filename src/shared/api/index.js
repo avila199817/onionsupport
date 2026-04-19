58761a434a3aa40665cd6818e87fbe914daf91b4
avila199817 <cristian@onionsupport.com>
2026-04-19 21:26:28 +0000

    Update index.js

--- src/shared/api/index.js
+++ src/shared/api/index.js
@@ -1,1 +1,22 @@
+export {
+  safeText,
+  safeNumber,
+  isPlainObject,
+  isEmptyQueryValue,
+  serializePrimitive,
+  flattenQueryObject,
+  cleanQueryParams,
+  mergeQueryParams,
+  buildListQuery,
+  toURLSearchParams,
+  toQueryString,
+} from "./query.js";
 
+export {
+  createCollectionApi,
+  default as createCollectionApiDefault,
+  normalizeCollectionResponse,
+  normalizeDetailResponse,
+  extractCollectionItems,
+  extractCollectionTotal,
+} from "./collectionApi.js";

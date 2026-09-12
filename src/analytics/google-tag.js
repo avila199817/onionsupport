1f6eec78c91f18d80f895a6e48ad0954e19a9e5f
Onion Support <cristian@onionsupport.com>
2026-09-12 21:51:00 +0200

    feat(ads): registrar lead de formulario en Google Ads

--- src/analytics/google-tag.js
+++ src/analytics/google-tag.js
@@ -10,8 +10,8 @@ import { PUBLIC_LEGAL } from "../core/public-legal.js";
 
   const GOOGLE_ANALYTICS_TAG_ID = "G-RQ77310QBH";
   const GOOGLE_ADS_TAG_ID = "AW-18395700376";
-  const CONTACT_CONVERSION_DESTINATION =
-    "AW-18395700376/WjQvCIe1tuMcEJi54MNE";
+  const LEAD_FORM_CONVERSION_DESTINATION =
+    "AW-18395700376/tEy-CIKov_UcEJi54MNE";
   const WHATSAPP_CONVERSION_DESTINATION =
     "AW-18395700376/6zBcCL3zo-ccEJi54MNE";
 
@@ -580,7 +580,10 @@ import { PUBLIC_LEGAL } from "../core/public-legal.js";
     trackAnalyticsEvent("generate_lead", {
       method: "public_support_form",
     });
-    sendGoogleAdsConversion(CONTACT_CONVERSION_DESTINATION);
+    sendGoogleAdsConversion(LEAD_FORM_CONVERSION_DESTINATION, {
+      value: 1.0,
+      currency: "EUR",
+    });
   });
 
   window.addEventListener(LEGACY_ANALYTICS_CONSENT_EVENT, () => {
